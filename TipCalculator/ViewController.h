3e721ae5575c6b170a3352b67219c1562412211f
zhonglis <zhonglis@gsc24.local>
2017-10-21 17:14:37 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by zhonglis on 10/21/17.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TipCalculator
+//
+//  Created by zhonglis on 10/21/17.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
