16398135a45bb6100916995e0f1d8f1b65327f8b
sdcwxy <zhonglinsun517@gmail.com>
2017-10-22 22:39:04 -0700

    First commit.

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControl;
+@property (weak, nonatomic) IBOutlet UILabel *savedAmount;
+@property (weak, nonatomic) IBOutlet UILabel *totalAmount;
+
 
 @end
 
@@ -19,10 +24,32 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)didTapSegment:(id)sender {
+    [self updateValue];
+}
+
+- (IBAction)didTapBackground:(id)sender {
+    [self updateValue];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)updateValue
+{
+    [self.view endEditing:YES];
+    
+    CGFloat billAmount = [self.textField.text floatValue];
+    
+    NSArray *tipValues = @[@(0.1),@(0.15),@(0.2)];
+    
+    CGFloat tip = billAmount * [tipValues[[self.segmentControl selectedSegmentIndex]] floatValue];
+    
+    self.savedAmount.text = [NSString stringWithFormat:@"$%10.2f",tip];
+    self.totalAmount.text = [NSString stringWithFormat:@"$%10.2f",tip + billAmount];
 }
 
 
